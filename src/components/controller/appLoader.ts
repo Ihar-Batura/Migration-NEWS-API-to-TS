e73a21ac4a82815b30f4a967ce8bb6649c5840d4
Ihar-Batura <A17331582@gmail.com>
2024-10-06 11:44:58 +0300

    fix: fix super in appLoader

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,10 +2,15 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL as string, {
-            apiKey: process.env.API_KEY as string,
+        super('https://rss-news-api.onrender.com/', {
+            apiKey: '05d9a68dbf48401fa68efaaaa9ae26a8',
         });
     }
 }
 
 export default AppLoader;
+
+// Есть 2 варианта как сделать super + еще нужно будет поменять API URL для корректной работы на gh-pages
+// super(process.env.API_URL as string, {
+//     apiKey: process.env.API_KEY as string,
+// });
